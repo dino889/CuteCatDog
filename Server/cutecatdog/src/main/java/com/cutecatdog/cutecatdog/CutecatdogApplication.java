1e6c01412fb45751448fdaa0f988f24878402d69
김수호 <is_null@kakao.com>
2022-03-07 15:39:52 +0900

    [BACK-INIT] Project Init

---
+++ Server/cutecatdog/src/main/java/com/cutecatdog/cutecatdog/CutecatdogApplication.java
@@ -0,0 +1,13 @@
+package com.cutecatdog.cutecatdog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CutecatdogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CutecatdogApplication.class, args);
+	}
+
+}
