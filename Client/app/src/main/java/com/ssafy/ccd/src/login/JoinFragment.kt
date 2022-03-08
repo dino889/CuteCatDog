5ea56b1ea7a143bbcd4535f6ad17f0b70109f7ef
LeeBoyeon <61018094+leeboyeon@users.noreply.github.com>
2022-03-08 11:07:23 +0900

    [APP-FEAT] Join-layout

--- Client/app/src/main/java/com/ssafy/ccd/src/login/JoinFragment.kt
+++ Client/app/src/main/java/com/ssafy/ccd/src/login/JoinFragment.kt
@@ -1,5 +1,6 @@
 package com.ssafy.ccd.src.login
 
+import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -10,14 +11,23 @@ import com.ssafy.ccd.config.BaseFragment
 import com.ssafy.ccd.databinding.FragmentJoinBinding
 
 class JoinFragment : BaseFragment<FragmentJoinBinding>(FragmentJoinBinding::bind, R.layout.fragment_join) {
+    private lateinit var loginActivity: LoginActivity
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
         }
     }
-
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        loginActivity = context as LoginActivity
+    }
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        binding.fragmentJoinBtnJoin.setOnClickListener {
+            loginActivity.openFragment(2)
+        }
     }
     companion object {
         @JvmStatic
