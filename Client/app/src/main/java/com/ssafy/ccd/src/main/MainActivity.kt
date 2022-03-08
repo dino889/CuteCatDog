a8d5958e506d4d43d8ab1a27603c9d5e1afcf12c
jiwoo <63338678+dino889@users.noreply.github.com>
2022-03-08 12:50:40 +0900

    [APP-FEAT] Init Bottom Navigation

--- Client/app/src/main/java/com/ssafy/ccd/src/main/MainActivity.kt
+++ Client/app/src/main/java/com/ssafy/ccd/src/main/MainActivity.kt
@@ -2,11 +2,24 @@ package com.ssafy.ccd.src.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
 import com.ssafy.ccd.R
+import com.ssafy.ccd.config.BaseActivity
+import com.ssafy.ccd.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity :BaseActivity<ActivityMainBinding>(ActivityMainBinding::inflate) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+//        setContentView(R.layout.activity_main)
+
+        // 네비게이션 호스트
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.activity_main_navHost) as NavHostFragment
+
+        // 네비게이션 컨트롤러
+        val navController = navHostFragment.navController
+
+        // 바인딩
+        NavigationUI.setupWithNavController(binding.activityMainBottomNavigationView, navController)
     }
 }
