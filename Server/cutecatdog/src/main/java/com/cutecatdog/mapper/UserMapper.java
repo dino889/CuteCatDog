2935f572ab28909337f489461927d33f2e7c7d07
김수호 <is_null@kakao.com>
2022-03-10 18:33:56 +0900

    [BACK-FIX] 프로젝트 폴더 구조 변경

--- Server/cutecatdog/src/main/java/com/cutecatdog/mapper/UserMapper.java
+++ Server/cutecatdog/src/main/java/com/cutecatdog/mapper/UserMapper.java
@@ -1,4 +1,4 @@
-package com.cutecatdog.cutecatdog.mapper;
+package com.cutecatdog.mapper;
 
 import java.sql.SQLException;
 
@@ -10,12 +10,19 @@ import org.apache.ibatis.annotations.Mapper;
 public interface UserMapper {
 
     int signup(UserDto userDto) throws SQLException;
+
     UserDto userInfo(int userId) throws SQLException;
+
     int modifyUser(UserDto userDto) throws SQLException;
+
     int withdrawal(int userId) throws SQLException;
+
     UserDto checkEmail(String email) throws SQLException;
+
     UserDto checkNickname(String nickname) throws SQLException;
+
     UserDto login(UserDto userDto) throws SQLException;
+
     int resetPw(UserDto userDto) throws SQLException;
-    
+
 }
