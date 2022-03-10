1505ed0381d4778459295c3882dd014fae476645
LeeBoyeon <61018094+leeboyeon@users.noreply.github.com>
2022-03-10 13:03:42 +0900

    [APP-FEAT] diary Fragment layout

--- Client/app/src/main/java/com/ssafy/ccd/src/main/diary/DiaryFragment.kt
+++ Client/app/src/main/java/com/ssafy/ccd/src/main/diary/DiaryFragment.kt
@@ -1,61 +1,45 @@
 package com.ssafy.ccd.src.main.diary
 
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.ssafy.ccd.R
 import com.ssafy.ccd.config.BaseFragment
 import com.ssafy.ccd.databinding.FragmentDiaryBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [DiaryFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class DiaryFragment : BaseFragment<FragmentDiaryBinding>(FragmentDiaryBinding::bind, R.layout.fragment_diary) {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
         }
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_diary, container, false)
-    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
+        binding.fragmentDiarySearch.setOnClickListener {
+            showSortDialog()
+        }
+    }
+    fun showSortDialog(){
+        val dialogView = LayoutInflater.from(requireContext()).inflate(R.layout.fragment_diary_sortbottom_sheet,null)
+        val dialog = BottomSheetDialog(requireContext())
+        dialog.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+        val param = WindowManager.LayoutParams()
+        param.width = WindowManager.LayoutParams.MATCH_PARENT
+        param.height = WindowManager.LayoutParams.WRAP_CONTENT
+        val window = dialog.window
+        dialog.setContentView(dialogView)
+        dialog.show()
+    }
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment DiaryFragment.
-         */
-        // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
             DiaryFragment().apply {
                 arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
                 }
             }
     }
---
+++ Client/app/src/main/java/com/ssafy/ccd/src/main/diary/DiaryWriteFragment.kt
@@ -0,0 +1,31 @@
+package com.ssafy.ccd.src.main.diary
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.ssafy.ccd.R
+import com.ssafy.ccd.config.BaseFragment
+import com.ssafy.ccd.databinding.FragmentDiaryWriteBinding
+
+class DiaryWriteFragment : BaseFragment<FragmentDiaryWriteBinding>(FragmentDiaryWriteBinding::bind,R.layout.fragment_diary_write) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+        }
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    companion object {
+        @JvmStatic
+        fun newInstance(param1: String, param2: String) =
+            DiaryWriteFragment().apply {
+                arguments = Bundle().apply {
+                }
+            }
+    }
+}
