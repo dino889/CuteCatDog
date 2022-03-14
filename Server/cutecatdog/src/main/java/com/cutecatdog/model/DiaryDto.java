4151bbdbb141b2b92e03d70ce99180a8a0865899
김다예 <ek03147@gmail.com>
2022-03-14 13:59:32 +0900

    [BACK-FIX] fix diary model

--- Server/cutecatdog/src/main/java/com/cutecatdog/model/DiaryDto.java
+++ Server/cutecatdog/src/main/java/com/cutecatdog/model/DiaryDto.java
@@ -1,5 +1,7 @@
 package com.cutecatdog.model;
 
+import java.util.List;
+
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
@@ -17,4 +19,8 @@ public class DiaryDto {
     private String title;
     @ApiModelProperty(value = "내용")
     private String content;
+    @ApiModelProperty(value = "해시태그")
+    private List<HashtagDto> hashtag;
+    @ApiModelProperty(value = "사진")
+    private List<PhotoDto> photo;
 }
