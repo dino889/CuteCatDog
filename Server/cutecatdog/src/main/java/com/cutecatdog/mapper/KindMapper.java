00ab90308b8fc644fe4b9d070e2e1fe5b581253d
1ComputerMaster <87481266+1ComputerMaster@users.noreply.github.com>
2022-03-14 13:08:02 +0900

    back/feat/kind-CRUD : 품종 부분 등록, 삭제, 수정, 보기 구현

---
+++ Server/cutecatdog/src/main/java/com/cutecatdog/mapper/KindMapper.java
@@ -0,0 +1,23 @@
+package com.cutecatdog.mapper;
+
+import java.sql.SQLException;
+import java.util.List;
+
+import com.cutecatdog.model.KindDto;
+
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface KindMapper {
+
+  public List<KindDto> selectKind() throws SQLException;
+
+  public boolean insertKind(String name) throws SQLException;
+
+  public Integer selectKindName(String name) throws SQLException;
+
+  public boolean updateKind(KindDto kindDto) throws SQLException;
+
+  public int deleteKind(int id) throws SQLException;
+  
+}
