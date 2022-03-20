4461b19217c782edf5c52a9ccaa4878dcded4202
jiwoo <63338678+dino889@users.noreply.github.com>
2022-03-20 14:50:03 +0900

    [APP-FEAT] 사용자 비밀번호 변경 레이아웃 추가
    #S06P22D103-106

---
+++ Client/app/src/main/java/com/ssafy/ccd/src/login/ResetPasswordFragment.kt
@@ -0,0 +1,33 @@
+package com.ssafy.ccd.src.login
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.ssafy.ccd.R
+import com.ssafy.ccd.config.BaseFragment
+import com.ssafy.ccd.databinding.FragmentResetPasswordBinding
+
+
+class ResetPasswordFragment : BaseFragment<FragmentResetPasswordBinding>(FragmentResetPasswordBinding::bind, R.layout.fragment_reset_password) {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_reset_password, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+}
