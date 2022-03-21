e3b565ba26f2130c7424d666ce7a69e69499fa94
김다예 <ek03147@gmail.com>
2022-03-21 13:56:43 +0900

    [BACK-FEAT] Schedule: Service

---
+++ Server/cutecatdog/src/main/java/com/cutecatdog/service/ScheduleService.java
@@ -0,0 +1,23 @@
+package com.cutecatdog.service;
+
+import java.util.List;
+
+import com.cutecatdog.model.Calendar.ScheduleDto;
+
+public interface ScheduleService {
+
+    boolean addSchedule(ScheduleDto scheduleDto) throws Exception;
+
+    List<ScheduleDto> findSchedule(int userId) throws Exception;
+    
+    List<ScheduleDto> findSchedulePet(int petId) throws Exception;
+    
+    List<ScheduleDto> findScheduleIno(int petId) throws Exception;
+    
+    List<ScheduleDto> findScheduleWalk(int petId) throws Exception;
+
+    boolean modifySchedule(ScheduleDto scheduleDto) throws Exception;
+
+    boolean removeSchedule(int id) throws Exception;
+
+}
---
+++ Server/cutecatdog/src/main/java/com/cutecatdog/service/ScheduleServiceImpl.java
@@ -0,0 +1,53 @@
+package com.cutecatdog.service;
+
+import java.util.List;
+
+import com.cutecatdog.mapper.ScheduleMapper;
+import com.cutecatdog.model.Calendar.ScheduleDto;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ScheduleServiceImpl implements ScheduleService{
+
+    @Autowired
+    SqlSession sqlSession;
+
+    @Override
+    public boolean addSchedule(ScheduleDto scheduleDto) throws Exception {
+        return sqlSession.getMapper(ScheduleMapper.class).insertSchedule(scheduleDto);
+    }
+
+    @Override
+    public List<ScheduleDto> findSchedule(int userId) throws Exception {
+        return sqlSession.getMapper(ScheduleMapper.class).selectSchedule(userId);
+    }
+
+    @Override
+    public List<ScheduleDto> findSchedulePet(int petId) throws Exception {
+        return sqlSession.getMapper(ScheduleMapper.class).selectSchedulePet(petId);
+    }
+
+    @Override
+    public List<ScheduleDto> findScheduleIno(int petId) throws Exception {
+        return sqlSession.getMapper(ScheduleMapper.class).selectScheduleIno(petId);
+    }
+
+    @Override
+    public List<ScheduleDto> findScheduleWalk(int petId) throws Exception {
+        return sqlSession.getMapper(ScheduleMapper.class).selectScheduleWalk(petId);
+    }
+
+    @Override
+    public boolean modifySchedule(ScheduleDto scheduleDto) throws Exception {
+        return sqlSession.getMapper(ScheduleMapper.class).updateSchedule(scheduleDto);
+    }
+
+    @Override
+    public boolean removeSchedule(int id) throws Exception {
+        return sqlSession.getMapper(ScheduleMapper.class).deleteSchedule(id);
+    }
+    
+}
