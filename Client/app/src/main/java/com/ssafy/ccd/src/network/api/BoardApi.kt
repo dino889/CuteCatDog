30404a4dc9fa317fcf80e5bf7418faaeafcbc1a4
jiwoo <63338678+dino889@users.noreply.github.com>
2022-03-23 11:21:01 +0900

    [APP-FEAT] 커뮤니티 관련 api, service 추가

--- Client/app/src/main/java/com/ssafy/ccd/src/network/api/BoardApi.kt
+++ Client/app/src/main/java/com/ssafy/ccd/src/network/api/BoardApi.kt
@@ -28,6 +28,10 @@ interface BoardApi {
     @DELETE("/board/{id}")
     suspend fun deletePost(@Path("id") id: Int) : Response<Message>
 
+    // 게시글에 해당하는 댓글 리스트만 조회
+    @GET("/board/{id}/comment")
+    suspend fun selectCommentList(@Path("id") id: Int) : Response<Message>
+
     // 댓글 등록
     @POST("/board/comment")
     suspend fun insertComment(@Body commentRequestDto : Comment) : Response<Message>
@@ -43,6 +47,14 @@ interface BoardApi {
     // 대댓글 등록
     @POST("/board/comment/add")
     suspend fun insertReply(@Body commentAddShowRequestDto: Comment) : Response<Message>
+
+    // 게시글 1개에 대해 로그인한 유저가 좋아요 눌렀는 지 확인
+    @GET("/board/isLike")
+    suspend fun selectPostIsLike(@Query("boardId") boardId: Int, @Query("userId") userId: Int) : Response<Message>
+
+    // 게시글 좋아요 등록 및 삭제
+    @POST("/board/likes")
+    suspend fun insertOrDeletePostLike(@Body likeRequestDto: Board) : Response<Message>
 
     // 게시글 타입별 게시글 조회
     @GET("/board/type/{typeId}")
