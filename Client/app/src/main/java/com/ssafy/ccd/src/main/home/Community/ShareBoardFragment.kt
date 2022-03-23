a882a4d6966f83e6c0fbf575421b6019b8547fd2
jiwoo <63338678+dino889@users.noreply.github.com>
2022-03-23 22:03:56 +0900

    [APP-FEAT] 공유해 게시판 레이아웃, 아이템 생성

--- Client/app/src/main/java/com/ssafy/ccd/src/main/home/Community/ShareBoardFragment.kt
+++ Client/app/src/main/java/com/ssafy/ccd/src/main/home/Community/ShareBoardFragment.kt
@@ -5,11 +5,16 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.ssafy.ccd.R
 import com.ssafy.ccd.config.BaseFragment
-import com.ssafy.ccd.databinding.FragmentShareBoardBinding
+import com.ssafy.ccd.databinding.FragmentLocalBoardBinding
+import com.ssafy.ccd.src.main.home.BoardAdapter
+import kotlinx.coroutines.runBlocking
 
-class ShareBoardFragment : BaseFragment<FragmentShareBoardBinding>(FragmentShareBoardBinding::bind,R.layout.fragment_share_board) {
+class ShareBoardFragment : BaseFragment<FragmentLocalBoardBinding>(FragmentLocalBoardBinding::bind,R.layout.fragment_local_board) {
+    private lateinit var localBoardAdapter: LocalBoardAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
@@ -18,14 +23,19 @@ class ShareBoardFragment : BaseFragment<FragmentShareBoardBinding>(FragmentShare
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        initRecyclerView()
     }
 
-    companion object {
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            ShareBoardFragment().apply {
-                arguments = Bundle().apply {
-                }
-            }
+    private fun initRecyclerView() {
+        runBlocking {
+            mainViewModel.getPostListByType(1)
+        }
+        mainViewModel.locPostList.observe(viewLifecycleOwner, {
+            binding.localBoardFragmentRvPostList.layoutManager = LinearLayoutManager(context, LinearLayoutManager.VERTICAL, false)
+            localBoardAdapter = LocalBoardAdapter(it, mainViewModel.allUserList.value!!, mainViewModel.likePostsByUserId.value!!, requireContext())
+            binding.localBoardFragmentRvPostList.adapter = localBoardAdapter
+        })
     }
+
+
 }
