c3f9e8f694df19aaf5477d82eda429a1008671d8
jiwoo <63338678+dino889@users.noreply.github.com>
2022-03-24 11:50:54 +0900

    [APP-FEAT] '울동네' 게시글 삭제 기능 구현

--- Client/app/src/main/java/com/ssafy/ccd/src/main/home/Community/LocalBoardAdapter.kt
+++ Client/app/src/main/java/com/ssafy/ccd/src/main/home/Community/LocalBoardAdapter.kt
@@ -1,29 +1,36 @@
 package com.ssafy.ccd.src.main.home.Community
 
 import android.content.Context
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.util.Log
+import android.view.*
+import android.widget.PopupMenu
+import androidx.core.os.bundleOf
+import androidx.core.view.isVisible
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.RecyclerView
 import com.ssafy.ccd.R
+import com.ssafy.ccd.config.ApplicationClass
 import com.ssafy.ccd.databinding.ItemLocalListBinding
 import com.ssafy.ccd.src.dto.Board
 import com.ssafy.ccd.src.dto.User
+import com.ssafy.ccd.src.network.service.BoardService
 
 class LocalBoardAdapter (var postList : MutableList<Board>, val userList: MutableList<User>, val userLikePost: MutableList<Int>, val context: Context) : RecyclerView.Adapter<LocalBoardAdapter.LocalBoardViewHolder>(){
 
     inner class LocalBoardViewHolder(private val binding: ItemLocalListBinding) : RecyclerView.ViewHolder(binding.root) {
-        val localPostItem = binding.localPost
+        val heartBtn = binding.fragmentLocalboardHeart
+        val commentBtn = binding.fragmentLocalboardChat
+        val moreBtn = binding.localItemBtnMore
 
         fun bindInfo(post: Board) {
-            for (user in userList) {
+            for (user in userList) {    // 작성자 nickname, profileImg 세팅
                 if(post.userId == user.id) {
                     binding.writer = user
                 }
             }
 
-            for (i in userLikePost) {
+            for (i in userLikePost) {   // 로그인 유저가 좋아요 누른 게시글 표시
                 if(post.id == i) {
                     binding.fragmentLocalboardHeart.progress = 0.5F
                 } else {
@@ -31,11 +38,12 @@ class LocalBoardAdapter (var postList : MutableList<Board>, val userList: Mutabl
                 }
             }
 
+            moreBtn.isVisible = post.userId == ApplicationClass.sharedPreferencesUtil.getUser().id
+
             binding.post = post
             binding.executePendingBindings()
         }
     }
-
 
 
 
@@ -48,8 +56,33 @@ class LocalBoardAdapter (var postList : MutableList<Board>, val userList: Mutabl
             bindInfo(postList[position])
             setIsRecyclable(false)
 
-             localPostItem.setOnClickListener {
-                itemClickListener.onClick(it, position)
+            heartBtn.setOnClickListener {
+                heartItemClickListener.onClick(it, position)
+            }
+
+            commentBtn.setOnClickListener {
+                commentItemClickListener.onClick(it, position)
+            }
+
+            moreBtn.setOnClickListener {
+                val popup = PopupMenu(context, moreBtn)
+                MenuInflater(context).inflate(R.menu.popup_menu, popup.menu)
+
+                popup.show()
+                popup.setOnMenuItemClickListener {
+                    when(it.itemId) {
+                        R.id.modify -> {
+                            modifyItemClickListener.onClick(postList[position].id)
+                            return@setOnMenuItemClickListener true
+                        }
+                        R.id.delete -> {
+                            deleteItemClickListener.onClick(postList[position].id)
+                            return@setOnMenuItemClickListener true
+                        } else -> {
+                            return@setOnMenuItemClickListener false
+                        }
+                    }
+                }
             }
         }
     }
@@ -62,10 +95,32 @@ class LocalBoardAdapter (var postList : MutableList<Board>, val userList: Mutabl
         fun onClick(view: View, position: Int)
     }
 
-    private lateinit var itemClickListener : ItemClickListener
+    private lateinit var heartItemClickListener : ItemClickListener
 
-    fun setItemClickListener(itemClickListener: ItemClickListener) {
-        this.itemClickListener = itemClickListener
+    fun setHeartItemClickListener(itemClickListener: ItemClickListener) {
+        this.heartItemClickListener = itemClickListener
+    }
+
+    private lateinit var commentItemClickListener : ItemClickListener
+
+    fun setCommentItemClickListener(itemClickListener: ItemClickListener) {
+        this.commentItemClickListener = itemClickListener
+    }
+
+    interface MenuClickListener {
+        fun onClick(postId: Int)
+    }
+
+    private lateinit var modifyItemClickListener : MenuClickListener
+
+    fun setModifyItemClickListener(menuClickListener: MenuClickListener) {
+        this.modifyItemClickListener = menuClickListener
+    }
+
+    private lateinit var deleteItemClickListener : MenuClickListener
+
+    fun setDeleteItemClickListener(menuClickListener: MenuClickListener) {
+        this.deleteItemClickListener = menuClickListener
     }
 
 }
--- Client/app/src/main/java/com/ssafy/ccd/src/main/home/Community/LocalBoardFragment.kt
+++ Client/app/src/main/java/com/ssafy/ccd/src/main/home/Community/LocalBoardFragment.kt
@@ -1,17 +1,20 @@
 package com.ssafy.ccd.src.main.home.Community
 
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.widget.PopupMenu
+import androidx.core.os.bundleOf
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.ssafy.ccd.R
 import com.ssafy.ccd.config.BaseFragment
 import com.ssafy.ccd.databinding.FragmentLocalBoardBinding
+import com.ssafy.ccd.src.dto.Message
 import com.ssafy.ccd.src.main.home.BoardAdapter
+import com.ssafy.ccd.src.network.service.BoardService
 import kotlinx.coroutines.runBlocking
+import retrofit2.Response
 
 /**
  * @author Jiwoo
@@ -34,6 +37,27 @@ class LocalBoardFragment : BaseFragment<FragmentLocalBoardBinding>(FragmentLocal
         writeBtnClickEvent()
     }
 
+    /**
+     * 뒤로가기 버튼 클릭 이벤트
+     */
+    private fun backBtnClickEvent() {
+        binding.fragmentLocalBack.setOnClickListener {
+            this@LocalBoardFragment.findNavController().popBackStack()
+        }
+    }
+
+    /**
+     * 글작성 완료 버튼 클릭 이벤트
+     */
+    private fun writeBtnClickEvent() {
+        binding.fragmentLocalWrite.setOnClickListener {
+            this@LocalBoardFragment.findNavController().navigate(R.id.action_localBoardFragment_to_writeLocalBoardFragment)
+        }
+    }
+
+    /**
+     * 게시글 recyclerView 초기화
+     */
     private fun initRecyclerView() {
         runBlocking {
             mainViewModel.getPostListByType(1)
@@ -42,19 +66,57 @@ class LocalBoardFragment : BaseFragment<FragmentLocalBoardBinding>(FragmentLocal
             binding.localBoardFragmentRvPostList.layoutManager = LinearLayoutManager(context, LinearLayoutManager.VERTICAL, false)
             localBoardAdapter = LocalBoardAdapter(it, mainViewModel.allUserList.value!!, mainViewModel.likePostsByUserId.value!!, requireContext())
             binding.localBoardFragmentRvPostList.adapter = localBoardAdapter
+
+            localBoardAdapter.setHeartItemClickListener(object : LocalBoardAdapter.ItemClickListener {
+                override fun onClick(view: View, position: Int) {
+                    // boardlike 호출 -> 색 변경
+                }
+            })
+
+            localBoardAdapter.setCommentItemClickListener(object : LocalBoardAdapter.ItemClickListener {
+                override fun onClick(view: View, position: Int) {
+                    // postId 포함해서 commentList 페이지로 이동
+                }
+            })
+
+
+            localBoardAdapter.setModifyItemClickListener(object : LocalBoardAdapter.MenuClickListener {
+                override fun onClick(postId: Int) {
+                    this@LocalBoardFragment.findNavController().navigate(R.id.action_localBoardFragment_to_writeLocalBoardFragment,
+                        bundleOf("postId" to postId)
+                    )
+                }
+            })
+
+            localBoardAdapter.setDeleteItemClickListener(object : LocalBoardAdapter.MenuClickListener {
+                override fun onClick(postId: Int) {
+                    deletePost(postId)
+                }
+            })
+
         })
     }
 
-    private fun backBtnClickEvent() {
-        binding.fragmentLocalBack.setOnClickListener {
-            this@LocalBoardFragment.findNavController().popBackStack()
+    private fun deletePost(postId: Int) {
+        var response : Response<Message>
+        runBlocking {
+            response = BoardService().deletePost(postId)
+        }
+        if(response.code() == 200 || response.code() == 500) {
+            val res = response.body()
+            if(res != null) {
+                if(res.success == true && res.data["isSuccess"] == true) {
+                    showCustomToast("게시글이 삭제되었습니다.")
+                    runBlocking {
+                        mainViewModel.getPostListByType(1)
+                    }
+                } else {
+                    showCustomToast("게시글 삭제 실패")
+                }
+            }
         }
     }
 
-    private fun writeBtnClickEvent() {
-        binding.fragmentLocalWrite.setOnClickListener {
-            this@LocalBoardFragment.findNavController().navigate(R.id.action_localBoardFragment_to_writeLocalBoardFragment)
-        }
-    }
+
 
 }
