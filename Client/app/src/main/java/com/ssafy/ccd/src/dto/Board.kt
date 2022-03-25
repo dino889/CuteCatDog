01e673d86ca4f4c66be6abb1d289019416cc9677
jiwoo <63338678+dino889@users.noreply.github.com>
2022-03-25 14:38:54 +0900

    [APP-FIX] 모든 게시판 댓글 수 세팅 오류 해결

--- Client/app/src/main/java/com/ssafy/ccd/src/dto/Board.kt
+++ Client/app/src/main/java/com/ssafy/ccd/src/dto/Board.kt
@@ -10,9 +10,10 @@ data class Board(
     val commentList : List<Comment>,
     val count: Int,
     val time: String,
-    val photoPath: String
+    val photoPath: String,
+    val commentCnt: Int
 ) {
-    constructor(userId: Int, typeId: Int, author: String, title: String, content: String, time: String, photoPath: String) : this(0, userId, typeId, author, title, content, listOf(), 0, time, photoPath) // insert
-    constructor(id: Int, typeId: Int, title: String, content: String, photoPath: String) : this(id, 0, typeId, "",  title, content, listOf(), 0, "", photoPath)    // 게시글 수정 update
-    constructor(id: Int, userId: Int) : this(id, userId, 0, "",  "", "", listOf(), 0, "", "")    // 게시글 좋아요 클릭
+    constructor(userId: Int, typeId: Int, author: String, title: String, content: String, time: String, photoPath: String) : this(0, userId, typeId, author, title, content, listOf(), 0, time, photoPath, 0) // insert
+    constructor(id: Int, typeId: Int, title: String, content: String, photoPath: String) : this(id, 0, typeId, "",  title, content, listOf(), 0, "", photoPath, 0)    // 게시글 수정 update
+    constructor(id: Int, userId: Int) : this(id, userId, 0, "",  "", "", listOf(), 0, "", "", 0)    // 게시글 좋아요 클릭
 }
