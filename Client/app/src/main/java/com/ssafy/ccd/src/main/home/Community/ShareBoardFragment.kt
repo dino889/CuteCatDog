e69601bc1ac145b51e12947ae972ba342348190b
jiwoo <63338678+dino889@users.noreply.github.com>
2022-03-28 14:58:17 +0900

    [APP-FEAT] '공유해' 게시글 삭제 기능 구현

--- Client/app/src/main/java/com/ssafy/ccd/src/main/home/Community/ShareBoardFragment.kt
+++ Client/app/src/main/java/com/ssafy/ccd/src/main/home/Community/ShareBoardFragment.kt
@@ -16,8 +16,11 @@ import com.ssafy.ccd.config.BaseFragment
 import com.ssafy.ccd.databinding.FragmentLocalBoardBinding
 import com.ssafy.ccd.databinding.FragmentShareBoardBinding
 import com.ssafy.ccd.src.dto.LikeRequestDto
+import com.ssafy.ccd.src.dto.Message
 import com.ssafy.ccd.src.main.home.BoardAdapter
+import com.ssafy.ccd.src.network.service.BoardService
 import kotlinx.coroutines.runBlocking
+import retrofit2.Response
 
 /**
  * @author Jiwoo
@@ -97,21 +100,51 @@ class ShareBoardFragment : BaseFragment<FragmentShareBoardBinding>(FragmentShare
 
         shareBoardAdapter.setDeleteItemClickListener(object : ShareBoardAdapter.MenuClickListener {
             override fun onClick(postId: Int, position: Int) {
-//                deletePost(postId, position)
+                deletePost(postId, position)
             }
         })
     }
 
+    /**
+     * 뒤로가기 버튼 클릭 이벤트
+     */
     private fun backBtnClickEvent() {
         binding.shareFragmentIbBack.setOnClickListener {
             this@ShareBoardFragment.findNavController().popBackStack()
         }
     }
 
+    /**
+     * 게시글 작성 클릭 이벤트
+     */
     private fun writeBtnClickEvent() {
         binding.shareFragmentIbWrite.setOnClickListener {
             this@ShareBoardFragment.findNavController().navigate(R.id.action_shareBoardFragment_to_writeShareBoardFragment,
                 bundleOf("postId" to -1))
+        }
+    }
+
+    /**
+     * 게시글 삭제 response
+     */
+    private fun deletePost(postId: Int, position: Int) {
+        var response : Response<Message>
+        runBlocking {
+            response = BoardService().deletePost(postId)
+        }
+        if(response.code() == 200 || response.code() == 500) {
+            val res = response.body()
+            if(res != null) {
+                if(res.success == true && res.data["isSuccess"] == true) {
+                    showCustomToast("게시글이 삭제되었습니다.")
+                    runBlocking {
+                        mainViewModel.getPostListByType(3)
+                    }
+                    shareBoardAdapter.notifyDataSetChanged()
+                } else {
+                    showCustomToast("게시글 삭제 실패")
+                }
+            }
         }
     }
 
