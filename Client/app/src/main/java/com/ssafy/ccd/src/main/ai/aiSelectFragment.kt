71503721184e16a5d59d4da69fee90e0dc72b725
LeeBoyeon <61018094+leeboyeon@users.noreply.github.com>
2022-03-31 14:24:39 +0900

    [APP-FIX] app-ai fragment design modify

--- Client/app/src/main/java/com/ssafy/ccd/src/main/ai/aiSelectFragment.kt
+++ Client/app/src/main/java/com/ssafy/ccd/src/main/ai/aiSelectFragment.kt
@@ -1,6 +1,7 @@
 package com.ssafy.ccd.src.main.ai
 
 import android.app.ProgressDialog
+import android.content.Context
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
@@ -8,6 +9,7 @@ import android.widget.ImageButton
 import android.widget.ImageView
 import android.widget.RadioGroup
 import androidx.navigation.fragment.findNavController
+import com.bumptech.glide.Glide
 import com.ssafy.ccd.R
 import com.ssafy.ccd.config.BaseFragment
 import com.ssafy.ccd.databinding.FragmentAiSelectBinding
@@ -20,20 +22,30 @@ class aiSelectFragment : BaseFragment<FragmentAiSelectBinding>(FragmentAiSelectB
     private lateinit var analygyBtn : Button
     private lateinit var backBtn : ImageButton
     private lateinit var radioGroup: RadioGroup
-
+    private lateinit var mainActivity:MainActivity
     private lateinit var mainViewModels: MainViewModels
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
 
+    }
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        mainActivity = context as MainActivity
+    }
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
+        mainActivity.hideBottomNavi(true)
         setInstance()
         setListener()
         setInit()
     }
 
     private fun setInit() {
-        imageView.setImageURI(mainViewModels.uploadedImageUri)
-        (requireActivity() as MainActivity).hideBottomAppBar()
+        Glide.with(requireContext())
+            .load(mainViewModels.uploadedImageUri)
+            .circleCrop()
+            .into(imageView)
+//        imageView.setImageURI(mainViewModels.uploadedImageUri)
     }
 
     private fun setListener() {
@@ -44,11 +56,20 @@ class aiSelectFragment : BaseFragment<FragmentAiSelectBinding>(FragmentAiSelectB
         analygyBtn.setOnClickListener {
             this@aiSelectFragment.findNavController().navigate(R.id.aiFragment)
         }
-
-        radioGroup.setOnCheckedChangeListener { _, i ->
-            if(i == R.id.fragmentAiSelect_rbDog) mainViewModels.aiType = 0
-            if(i == R.id.fragmentAiSelect_rbCat) mainViewModels.aiType = 1
+        binding.fragmentSelectAiDog.setOnClickListener {
+            binding.fragmentSelectAiDogCheck.visibility = View.VISIBLE
+            binding.fragmentSelectAiCatCheck.visibility = View.INVISIBLE
+            mainViewModels.aiType = 0
         }
+        binding.fragmentSelectAiCat.setOnClickListener {
+            binding.fragmentSelectAiCatCheck.visibility = View.VISIBLE
+            binding.fragmentSelectAiDogCheck.visibility = View.INVISIBLE
+            mainViewModels.aiType = 1
+        }
+//        radioGroup.setOnCheckedChangeListener { _, i ->
+//            if(i == R.id.fragmentAiSelect_rbDog) mainViewModels.aiType = 0
+//            if(i == R.id.fragmentAiSelect_rbCat) mainViewModels.aiType = 1
+//        }
     }
 
     private fun setInstance() {
@@ -56,21 +77,27 @@ class aiSelectFragment : BaseFragment<FragmentAiSelectBinding>(FragmentAiSelectB
         analygyBtn = binding.fragmentAiSelectBtnAnaylsis
         mainViewModels = (requireActivity() as MainActivity).mainViewModels
         backBtn = binding.fragmentAiSelectIvBack
-        radioGroup = binding.fragmentAiSelectRg
+//        radioGroup = binding.fragmentAiSelectRg
     }
 
     override fun onDestroy() {
         super.onDestroy()
-        (requireActivity() as MainActivity).showBottomAppBar()
+//        (requireActivity() as MainActivity).showBottomAppBar()
     }
 
     override fun onPause() {
         super.onPause()
-        (requireActivity() as MainActivity).showBottomAppBar()
+//        (requireActivity() as MainActivity).showBottomAppBar()
     }
 
     override fun onResume() {
         super.onResume()
-        (requireActivity() as MainActivity).hideBottomAppBar()
+//        (requireActivity() as MainActivity).hideBottomAppBar()
+        mainActivity.hideBottomNavi(true)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        mainActivity.hideBottomNavi(true)
     }
 }
