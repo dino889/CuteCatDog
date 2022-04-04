02df911a2ce9dfed4be87411b0bf2c9f60e320bc
김수호 <is_null@kakao.com>
2022-04-04 14:33:26 +0900

    [BACK-UPDATE] REQUEST TYPE 변경

--- Server/cutecatdog/src/main/java/com/cutecatdog/controller/AIController.java
+++ Server/cutecatdog/src/main/java/com/cutecatdog/controller/AIController.java
@@ -7,11 +7,13 @@ import com.cutecatdog.service.File.FileService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -27,8 +29,8 @@ public class AIController {
   @Autowired
   private FileService fileService;
 
-  @PostMapping()
-  public ResponseEntity<Message> getAnimalKind(@RequestParam("file") MultipartFile file) {
+  @PostMapping(path = "", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+  public ResponseEntity<Message> getAnimalKind(@RequestPart("file") MultipartFile file) {
     Message msg = new Message();
     HttpStatus status = HttpStatus.OK;
 
