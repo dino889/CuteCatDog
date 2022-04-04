a81a5a70bcfa9177b97bc9b1de3e717d1bf080a0
김다예 <ek03147@gmail.com>
2022-04-04 16:14:33 +0900

    [BACK-FIX] 스케쥴링 기능 추가

--- Server/cutecatdog/src/main/java/com/cutecatdog/CutecatdogApplication.java
+++ Server/cutecatdog/src/main/java/com/cutecatdog/CutecatdogApplication.java
@@ -2,7 +2,9 @@ package com.cutecatdog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @SpringBootApplication
 public class CutecatdogApplication {
 
