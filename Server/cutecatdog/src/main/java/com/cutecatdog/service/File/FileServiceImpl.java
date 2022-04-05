380e4517578e48fb4bba65813cb26eacfd5f1590
김수호 <is_null@kakao.com>
2022-04-05 11:10:35 +0900

    [BACK-FEAT] APK download, UCC link 적용

--- Server/cutecatdog/src/main/java/com/cutecatdog/service/File/FileServiceImpl.java
+++ Server/cutecatdog/src/main/java/com/cutecatdog/service/File/FileServiceImpl.java
@@ -1,13 +1,20 @@
 package com.cutecatdog.service.File;
 
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.OutputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
 
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.util.FileCopyUtils;
 import org.springframework.web.multipart.MultipartFile;
 
 @Service
@@ -15,6 +22,9 @@ public class FileServiceImpl implements FileService {
 
   @Value("${file.dir}")
   private String targetDir;
+
+  @Value("${file.apk}")
+  private String apkPath;
 
   @Override
   public boolean saveFile(MultipartFile file) {
@@ -41,4 +51,20 @@ public class FileServiceImpl implements FileService {
     }
   }
 
+  @Override
+  public void downloadAPK(HttpServletResponse res) {
+    try {
+      File file = new File(apkPath);
+      BufferedInputStream bis = new BufferedInputStream(new FileInputStream(file));
+      res.setContentType("application/octet-stream");
+      res.setHeader("Content-Disposition", "attachment; filename=\"" + file.getName() + "\"");
+      FileCopyUtils.copy(bis, res.getOutputStream());
+      bis.close();
+      res.getOutputStream().flush();
+      res.getOutputStream().close();
+    } catch (Exception e) {
+      System.out.println(e.getMessage());
+    }
+
+  }
 }
